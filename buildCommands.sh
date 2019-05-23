c0fe0ec3b71b1691330e6c7a813deaf48cadff7d
Bjørn Grøva <bjorn.grova@brreg.no>
2019-05-23 15:11:35 +0200

    added build scripts for concept client

---
+++ buildCommands.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+i=0
+
+docker_apps="concept-client"
+
+for docker_app in $docker_apps; do
+    i=$((i+1))
+    BUILD_APPS[$i]=$docker_app
+    BUILD_CMD[$i]="( cd applications/$docker_app && ./buildDocker.sh )"
+done
+
+maven_apps=""
+
+for maven_app in $maven_apps; do
+    i=$((i+1))
+    BUILD_APPS[$i]=$maven_app
+    BUILD_CMD[$i]="mvn clean install -B -T 2C --projects applications/$maven_app --also-make"
+done
+
+export BUILD_APPS
+export BUILD_CMD
---
+++ concept-client/buildDocker.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+SECONDS=0 # start timer
+
+echo "Install dependencies"
+npm install
+
+echo "SECONDS"
+echo $SECONDS
+
+echo "Run tests"
+npm test
+
+echo "SECONDS"
+echo $SECONDS
+
+if [[ ! -z "$TRAVIS" ]]
+then
+    echo "Report coverage"
+    codecov
+fi
+
+echo "SECONDS"
+echo $SECONDS
+
+echo "Start build in docker image";
+docker build -t dcatno/concept-client:latest .
+
+echo "SECONDS"
+echo $SECONDS

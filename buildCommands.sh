c02822efb0cdfa7cbe21d98f7384e1483c428af7
Bjørn Grøva <bjorn.grova@brreg.no>
2019-05-23 15:18:44 +0200

    fixed directory structure

--- buildCommands.sh
+++ buildCommands.sh
@@ -7,7 +7,7 @@ docker_apps="concept-client"
 for docker_app in $docker_apps; do
     i=$((i+1))
     BUILD_APPS[$i]=$docker_app
-    BUILD_CMD[$i]="( cd applications/$docker_app && ./buildDocker.sh )"
+    BUILD_CMD[$i]="( cd $docker_app && ./buildDocker.sh )"
 done
 
 maven_apps=""
@@ -15,7 +15,7 @@ maven_apps=""
 for maven_app in $maven_apps; do
     i=$((i+1))
     BUILD_APPS[$i]=$maven_app
-    BUILD_CMD[$i]="mvn clean install -B -T 2C --projects applications/$maven_app --also-make"
+    BUILD_CMD[$i]="mvn clean install -B -T 2C --projects $maven_app --also-make"
 done
 
 export BUILD_APPS

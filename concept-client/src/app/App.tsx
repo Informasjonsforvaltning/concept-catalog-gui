80e8eed137bada9166b277b9e2e4d08311401b01
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-05-30 13:18:12 +0200

    chore(concept-client): refactor App

--- concept-client/src/app/App.tsx
+++ concept-client/src/app/App.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { Hello } from '../components/Hello';
 
-export const App = () => {
-  return <Hello compiler="Typescript" framework="React" bundler="Webpack" />;
-};
+export const App = () => (
+  <Router>
+    <Link to="/list">Link</Link>
+    <Switch>
+      <Route path="/" component={Hello} exact={true} />
+      <Route path="/list" component={Hello} exact={true} />
+    </Switch>
+  </Router>
+);

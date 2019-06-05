a87d1df7347399bdbd2668bde3a5c928d6d7c5cb
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-06-05 20:36:20 +0200

    chore(concept-client): remove babel

--- concept-client/webpack.dev.config.js
+++ concept-client/webpack.dev.config.js
@@ -27,11 +27,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader'
-      },
-      {
         test: /\.s?css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       },

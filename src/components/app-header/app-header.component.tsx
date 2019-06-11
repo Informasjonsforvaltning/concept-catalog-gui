17e17608e71d32521e8f642e503e597210fd919c
Gjermund Skobba <gjermund@skobba.net>
2019-06-11 22:06:05 +0200

    feat(concept-catalog-gui): Added header with account icon.

--- src/components/app-header/app-header.component.tsx
+++ src/components/app-header/app-header.component.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import './app-header.scss';
 import { localization } from '../../lib/localization';
+import { DropdownToggle, DropdownItem, DropdownMenu, UncontrolledDropdown, Nav, NavItem } from 'reactstrap';
 
 export const HeaderPure: FunctionComponent = (): JSX.Element => (
   <header>
@@ -15,27 +16,62 @@ export const HeaderPure: FunctionComponent = (): JSX.Element => (
     <div className="fdk-header">
       <div className="container">
         <div className="row">
-          <div className="col-6 col-md-4">
+          <div className="col-6 col-lg-4">
             <a title="Link til Felles datakatalog" href="/">
               <span className="uu-invisible" aria-hidden="false" />
               <img className="fdk-logo" src="./img/logo-registrering.svg" alt="Logo for Felles datakatalog" />
             </a>
           </div>
 
-          <div className="col-6 col-md-4 d-flex justify-content-center align-items-center">
+          <div className="col-4 d-none d-lg-block my-auto">
             <span>
-              <strong>{localization['title']}</strong>
+              <div className="fdk-title-header">{localization['title']}</div>
             </span>
           </div>
-          <div className="col-md-4 d-flex align-items-center fdk-header-text_items justify-content-end">
-            {/* TODO: CurrentUser */}
+
+          <div className="col-6 col-lg-4 d-flex align-items-center fdk-header-text_items justify-content-end">
+            <UncontrolledDropdown className="d-none d-lg-inline">
+              <DropdownToggle className="fdk-button-language" caret>
+                Språk
+              </DropdownToggle>
+              <DropdownMenu right className="fdk-dropdownmenu">
+                <DropdownItem>Norsk</DropdownItem>
+                <DropdownItem>Nynorsk</DropdownItem>
+                <DropdownItem>English</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+
+            <UncontrolledDropdown
+              tabIndex="0"
+              className="fdk-dropdown-menu d-inline d-lg-none justify-content-end text-right"
+            >
+              <DropdownToggle className="fdk-button fdk-button-menu" caret color="primary">
+                Meny
+              </DropdownToggle>
+
+              <DropdownMenu right className="fdk-dropdownmenu">
+                <DropdownItem>Konto</DropdownItem>
+                <DropdownItem>Logg ut</DropdownItem>
+                <DropdownItem>Norsk</DropdownItem>
+                <DropdownItem>Nynorsk</DropdownItem>
+                <DropdownItem>English</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+
+            <UncontrolledDropdown className="d-none d-lg-inline fdk-user-button">
+              <DropdownToggle className="fdk-button-account fdk-user" caret>
+                Kari Normann
+              </DropdownToggle>
+              <DropdownMenu right className="fdk-dropdownmenu">
+                <DropdownItem>Konto</DropdownItem>
+                <DropdownItem>Logg ut</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
           </div>
         </div>
       </div>
     </div>
   </header>
 );
-
-//export default ReactHeader;
 
 export const Header = HeaderPure;

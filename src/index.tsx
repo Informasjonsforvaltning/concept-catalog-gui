fcd5f05f08ced9803cb9b66974c85451e4ab5aa4
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-06-11 14:44:36 +0200

    chore: add config
    #tmp
    
    #tmp
    
    #tmp

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
+import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { loadConfig } from './config';
 import { App } from './app/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function AppRoot(): JSX.Element {
+  return <App />;
+}
+
+function render(): void {
+  ReactDOM.render(AppRoot(), document.getElementById('root'));
+}
+
+loadConfig()
+  .then(render)
+  .catch(console.error);

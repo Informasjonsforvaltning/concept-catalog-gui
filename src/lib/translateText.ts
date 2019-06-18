6276217248c576d87cbcf513bd8859c7b88798cf
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-06-18 21:42:47 +0200

    chore: getTranslate text change return type

--- src/lib/translateText.ts
+++ src/lib/translateText.ts
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 import { localization } from './localization';
 
-export const getTranslateText = (textObj: object, language?: string): string | null => {
+export const getTranslateText = (textObj: object, language?: string): string => {
   const selectedLanguage = language || localization.getLanguage();
   if (typeof textObj === 'string') {
     return textObj;
   }
 
   if (textObj === null || typeof textObj !== 'object') {
-    return null;
+    return '';
   }
 
   return (
@@ -18,6 +18,6 @@ export const getTranslateText = (textObj: object, language?: string): string | n
     _.get(textObj, 'no') ||
     _.get(textObj, 'nn') ||
     _.get(textObj, 'en') ||
-    null
+    ''
   );
 };

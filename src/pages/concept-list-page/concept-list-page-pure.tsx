ec9bb79c5f70b3b4f4d5b54de9087613caf4d1c0
Gjermund Skobba <gjermundskobba@Gjermunds-MacBook-Pro.local>
2019-06-18 13:47:36 +0200

    feat: Added title

--- src/pages/concept-list-page/concept-list-page-pure.tsx
+++ src/pages/concept-list-page/concept-list-page-pure.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import _ from 'lodash';
 import { ConceptList } from './concept-list/concept-list.component';
+import { ConceptTitle } from './concept-title/concept-title.component';
 
 interface Props {
   concepts: object;
@@ -9,6 +10,9 @@ interface Props {
 export const ConceptListPagePure = ({ concepts }: Props): JSX.Element => {
   return (
     <div className="container">
+      <div className="row mb-2">
+        <ConceptTitle title="Brønnøysundregisterne" />
+      </div>
       <div className="mb-2">
         <ConceptList items={_.get(concepts, 'concepts')} />
       </div>

c174a494ce978ba59f995a0ce7ea5f4a24ce391f
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-06-21 11:45:53 +0200

    chore: add tests list-item

---
+++ src/pages/concept-list-page/concept-list/list-item/list-item.component.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ListItem } from './list-item.component';
+
+let defaultProps;
+
+beforeEach(() => {
+  defaultProps = {
+    col1: 'column1',
+    col2: 'column2',
+    col3: 'column3',
+    path: '/tester'
+  };
+});
+
+test('should render ListItem with status DRAFT', (): void => {
+  const wrapper = shallow(<ListItem {...defaultProps} status="DRAFT" />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ListItem with status PUBLISH', (): void => {
+  const wrapper = shallow(<ListItem {...defaultProps} status="PUBLISH" />);
+  expect(wrapper).toMatchSnapshot();
+});

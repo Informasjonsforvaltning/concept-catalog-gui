9040d24f146dad91cedd9c24149ed3bffab90afa
Gjermund Skobba <gjermund@skobba.net>
2019-07-08 15:09:07 +0200

    feat: #1992 Add help text for all inputs.

--- src/pages/concept-registration-page/form-contactInfo/form-contactInfo-pure.component.tsx
+++ src/pages/concept-registration-page/form-contactInfo/form-contactInfo-pure.component.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { Form, Field } from 'formik';
 import { localization } from '../../../lib/localization';
 import { InputField } from '../../../components/field-input/field-input.component';
+import { HelpText } from '../../../components/help-text/help-text.component';
 
 export const FormContactInfoPure = (): JSX.Element => (
   <Form>
+    <HelpText
+      title={localization.kontaktinformasjonTitle}
+      required={false}
+      helpTextAbstract={localization.kontaktinformasjonAbstract}
+      helpTextDescription={localization.kontaktinformasjonDescription}
+    />
     <div className="form-group d-flex">
       <div className="w-50">
         <Field name="email" component={InputField} label={localization.email} showLabel={true} />

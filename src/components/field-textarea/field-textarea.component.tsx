c3b150f4e0185e48ccf9fc2b1719badf6b44f6aa
Gjermund Skobba <gjermund@skobba.net>
2019-07-09 14:28:55 +0200

    feat: #2029 Add row prop to set textarea rows.

--- src/components/field-textarea/field-textarea.component.tsx
+++ src/components/field-textarea/field-textarea.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 interface Props {
+  rows: number;
   field: {
     name: string;
   };
@@ -12,11 +13,11 @@ interface Props {
   label: string;
 }
 
-export const TextAreaField = ({ field, form: { touched, errors }, showLabel, label }: Props): JSX.Element => (
+export const TextAreaField = ({ field, form: { touched, errors }, showLabel, label, rows = 2 }: Props): JSX.Element => (
   <div className="pl-2">
     <label className="fdk-form-label w-100 fdk-text-strong" htmlFor={field.name}>
       {showLabel ? label : null}
-      <textarea {...field} className="form-control" />
+      <textarea {...field} rows={rows} className="form-control" />
     </label>
     {touched[field.name] && errors[field.name] && <div className="alert alert-danger mt-3">{errors[field.name]}</div>}
   </div>

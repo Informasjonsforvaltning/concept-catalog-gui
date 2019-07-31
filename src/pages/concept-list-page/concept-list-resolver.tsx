e2456a9d5e41c0366a59e10292da33a789976841
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-07-31 13:45:47 +0200

    chore: #2010 skip memoize as patching values should update list and reg-page

--- src/pages/concept-list-page/concept-list-resolver.tsx
+++ src/pages/concept-list-page/concept-list-resolver.tsx
@@ -3,12 +3,10 @@ import { resolve } from 'react-resolver';
 import { registrationApiGet } from '../../api/concept-catalogue-api';
 import { publisherApiGet } from '../../api/publisher-api';
 
-const memoizedRegistrationApiGet = _.memoize(registrationApiGet);
 const memoizedPublisherApiGetByOrgNr = _.memoize(publisherApiGet);
 
 const mapProps = {
-  concepts: props =>
-    memoizedRegistrationApiGet(`/begreper?orgNummer=${_.get(props, ['match', 'params', 'catalogId'])}`),
+  concepts: props => registrationApiGet(`/begreper?orgNummer=${_.get(props, ['match', 'params', 'catalogId'])}`),
   publisher: props => memoizedPublisherApiGetByOrgNr(`/publishers/${_.get(props, ['match', 'params', 'catalogId'])}`)
 };
 
--- src/pages/concept-registration-page/concept-registration-resolver.tsx
+++ src/pages/concept-registration-page/concept-registration-resolver.tsx
@@ -2,12 +2,10 @@ import _ from 'lodash';
 import { resolve } from 'react-resolver';
 import { getConcept } from '../../api/concept-registration-api';
 
-const memoizedGetConcept = _.memoize(getConcept);
-
 const mapProps = {
   concept: async props => {
     const conceptId = _.get(props, ['match', 'params', 'conceptId']);
-    const concepts = await Promise.resolve(memoizedGetConcept(`?orgNummer=${conceptId}`));
+    const concepts = await Promise.resolve(getConcept(`?orgNummer=${conceptId}`));
     return _.find(concepts, { id: conceptId });
   }
 };

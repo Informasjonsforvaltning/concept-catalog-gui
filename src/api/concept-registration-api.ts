cc69d7507cb226a9f93d6d24fd5ae1d92ccb6f95
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-08-01 09:17:42 +0200

    chore: refactor api

--- src/api/concept-registration-api.ts
+++ src/api/concept-registration-api.ts
@@ -1,11 +1,11 @@
 import { registrationApiGet, registrationApiPost, registrationApiPatch } from './concept-catalogue-api';
 
-export const conceptListPath = (): string => 'begreper/';
+export const conceptListPath = (): string => 'begreper';
 
-export const conceptPath = (conceptId): string => `${conceptListPath()}${conceptId}`;
+export const conceptPath = (conceptId): string => `${conceptListPath()}/${conceptId}`;
 
 export const getConcept = (catalogId): Promise<void> => registrationApiGet(conceptPath(catalogId));
 
-export const postConcept = (catalogId, body): Promise<void> => registrationApiPost(conceptPath(catalogId), body);
+export const postConcept = (body): Promise<void> => registrationApiPost(conceptListPath, body);
 
 export const patchConcept = (conceptId, patch): Promise<void> => registrationApiPatch(conceptPath(conceptId), patch);

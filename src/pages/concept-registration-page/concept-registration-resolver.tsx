df5eab965d79f06d8b053bc2fca5410c750ebea2
Gjermund Skobba <gjermund@skobba.net>
2019-08-09 13:49:09 +0200

    fix: Using the correct orgNummer.

--- src/pages/concept-registration-page/concept-registration-resolver.tsx
+++ src/pages/concept-registration-page/concept-registration-resolver.tsx
@@ -3,10 +3,8 @@ import { resolve } from 'react-resolver';
 import { getConcept } from '../../api/concept-registration-api';
 
 const mapProps = {
-  concept: async props => {
-    const conceptId = _.get(props, ['match', 'params', 'conceptId']);
-    const concepts = await Promise.resolve(getConcept(`?orgNummer=${conceptId}`));
-    return _.find(concepts, { id: conceptId });
+  concept: async ({ catalogId, conceptId }) => {
+    return _.find(await getConcept(`?orgNummer=${catalogId}`), { id: conceptId });
   }
 };
 

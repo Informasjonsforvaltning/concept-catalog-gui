70d7b2944aff602cac35a3ee0cb14810b7e5a3f8
Gjermund Skobba <gjermund@skobba.net>
2019-08-09 13:59:23 +0200

    fix: Correct patch url.

--- src/api/concept-registration-api.ts
+++ src/api/concept-registration-api.ts
@@ -9,7 +9,7 @@ export const conceptListPath = (): string => 'begreper';
 
 export const conceptPath = (conceptId): string => `${conceptListPath()}/${conceptId}`;
 
-export const getConcept = (catalogId): Promise<void> => registrationApiGet(conceptPath(catalogId));
+export const getConcept = (catalogId): Promise<any[]> => registrationApiGet(conceptPath(catalogId));
 
 export const postConcept = (body): Promise<void> => registrationApiPost(conceptListPath(), body);
 
--- src/pages/concept-registration-page/concept-registration-resolver.tsx
+++ src/pages/concept-registration-page/concept-registration-resolver.tsx
@@ -1,13 +1,9 @@
-import _ from 'lodash';
 import { resolve } from 'react-resolver';
 import { getConcept } from '../../api/concept-registration-api';
 
 const mapProps = {
-  concept: async props => {
-    const conceptId = _.get(props, ['match', 'params', 'conceptId']);
-    const concepts = await Promise.resolve(getConcept(`?orgNummer=${conceptId}`));
-    return _.find(concepts, { id: conceptId });
-  }
+  concept: async ({ catalogId, conceptId }) =>
+    (await getConcept(`?orgNummer=${catalogId}`)).find(concept => concept.id === conceptId)
 };
 
 export const ConceptRegistrationResolver = resolve(mapProps);

687806ce155e50035b076dac374f0b4b6f465bbe
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-08-20 16:39:58 +0200

    fix(concept-catalogue-gui): #2091 allow empty fields for omfang and contact information

--- src/pages/concept-registration-page/form-use-of-concept/form-useOfConcept.schema.tsx
+++ src/pages/concept-registration-page/form-use-of-concept/form-useOfConcept.schema.tsx
@@ -6,7 +6,11 @@ export const schema = Yup.object().shape({
   fagområde: Yup.string().min(2, localization.validationMin2),
   bruksområde: Yup.string().min(2, localization.validationMin2),
   omfang: Yup.object().shape({
-    tekst: Yup.string().min(2, localization.validationMin2),
-    uri: Yup.string().url(localization.validationUrl)
+    tekst: Yup.string()
+      .nullable()
+      .min(2, localization.validationMin2),
+    uri: Yup.string()
+      .nullable()
+      .url(localization.validationUrl)
   })
 });

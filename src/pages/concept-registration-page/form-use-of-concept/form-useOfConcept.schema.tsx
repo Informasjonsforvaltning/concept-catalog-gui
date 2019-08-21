0645f3acdc6393f965ae8b9ea39352d5157c9a92
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-08-21 11:38:36 +0200

    feat(concept-catalogue-gui): #2156 change bruksområde input field type to support tags

--- src/pages/concept-registration-page/form-use-of-concept/form-useOfConcept.schema.tsx
+++ src/pages/concept-registration-page/form-use-of-concept/form-useOfConcept.schema.tsx
@@ -4,7 +4,7 @@ import { localization } from '../../../lib/localization';
 export const schema = Yup.object().shape({
   eksempel: Yup.string().min(2, localization.validationMin2),
   fagområde: Yup.string().min(2, localization.validationMin2),
-  bruksområde: Yup.string().min(2, localization.validationMin2),
+  bruksområde: Yup.array().of(Yup.string().min(2, localization.validationMin2)),
   omfang: Yup.object().shape({
     tekst: Yup.string()
       .nullable()

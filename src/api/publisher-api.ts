d1a1ff4c6d779bc82a208291751b4379b279023b
Markko Paas <markko.paas@gmail.com>
2019-08-29 12:48:54 +0200

    refactor: Use axios in publisher-api

--- src/api/publisher-api.ts
+++ src/api/publisher-api.ts
@@ -1,28 +1,9 @@
-import url from 'url';
+import axios from 'axios';
 import { getConfig } from '../config';
 
-const getRootUrl = () => getConfig().publisherApi.host;
-const resolveUrl = path => url.resolve(getRootUrl(), path);
-
-const validateResponse = response => {
-  if (!response.ok) {
-    throw new Error(`Fetch error: ${response.status} ${response.statusText}`);
-  }
-  return response.json();
-};
-
-export const publisherApi = (method, path, jsonBody?) => {
-  const headers = { Accept: 'application/json' }; // required for cors
-  if (jsonBody) {
-    Object.assign(headers, { 'Content-Type': 'application/json' });
-  }
-
-  const body = jsonBody && JSON.stringify(jsonBody);
-  return fetch(resolveUrl(path), {
-    method,
-    headers: { authorization: getConfig().publisherApi.authorization },
-    body
-  }).then(validateResponse);
-};
-
-export const publisherApiGet = path => publisherApi('GET', path);
+export const publisherApiGet = path =>
+  axios
+    .get(`${getConfig().publisherApi.host}${path}`, {
+      headers: { authorization: getConfig().publisherApi.authorization }
+    })
+    .then(r => r.data);

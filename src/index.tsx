fe6a8baf5085f6c8a38f759118158a42d17b0b37
Markko Paas <markko.paas@gmail.com>
2019-08-29 11:49:13 +0200

    refactor: Remove dependency injection pattern as it is unjustified - no tests that would benefit from it

--- src/index.tsx
+++ src/index.tsx
@@ -2,30 +2,18 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { loadConfig, getConfig } from './config';
+import { loadConfig } from './config';
 import { App } from './app/App';
-import { configureRegistrationApi } from './api/concept-catalogue-api';
-import { configurePublisherApi } from './api/publisher-api';
 import { ErrorBoundary } from './components/error-boundary/error-boundary';
 
-function AppRoot(): JSX.Element {
-  return (
+const render = () =>
+  ReactDOM.render(
     <ErrorBoundary>
       <App />
-    </ErrorBoundary>
+    </ErrorBoundary>,
+    document.getElementById('root')
   );
-}
-
-async function configureServices() {
-  configureRegistrationApi(getConfig().conceptRegistrationApi);
-  configurePublisherApi(getConfig().publisherApi);
-}
-
-function render(): void {
-  ReactDOM.render(AppRoot(), document.getElementById('root'));
-}
 
 loadConfig()
-  .then(configureServices)
   .then(render)
   .catch(console.error);

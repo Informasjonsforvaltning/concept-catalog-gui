0a01295934d7634e870b32e31fe7ae1967755fe9
Markko Paas <markko.paas@gmail.com>
2019-08-29 13:09:37 +0200

    chore: Rename registrationApi to conceptCatalogueApi

--- src/api/concept-registration-api.ts
+++ src/api/concept-registration-api.ts
@@ -1,18 +1,19 @@
 import {
-  registrationApiGet,
-  registrationApiPost,
-  registrationApiPatch,
-  registrationApiDelete
+  conceptCatalogueApiGet,
+  conceptCatalogueApiPost,
+  conceptCatalogueApiPatch,
+  conceptCatalogueApiDelete
 } from './concept-catalogue-api';
 
 export const conceptListPath = (): string => '/begreper';
 
 export const conceptPath = (conceptId): string => `${conceptListPath()}/${conceptId}`;
 
-export const getConcept = (catalogId): Promise<any[]> => registrationApiGet(conceptPath(catalogId));
+export const getConcept = (catalogId): Promise<any[]> => conceptCatalogueApiGet(conceptPath(catalogId));
 
-export const postConcept = (body): Promise<void> => registrationApiPost(conceptListPath(), body);
+export const postConcept = (body): Promise<void> => conceptCatalogueApiPost(conceptListPath(), body);
 
-export const patchConcept = (conceptId, patch): Promise<void> => registrationApiPatch(conceptPath(conceptId), patch);
+export const patchConcept = (conceptId, patch): Promise<void> =>
+  conceptCatalogueApiPatch(conceptPath(conceptId), patch);
 
-export const deleteConcept = (conceptId: string): Promise<void> => registrationApiDelete(conceptPath(conceptId));
+export const deleteConcept = (conceptId: string): Promise<void> => conceptCatalogueApiDelete(conceptPath(conceptId));
--- src/pages/concept-list-page/concept-list-resolver.tsx
+++ src/pages/concept-list-page/concept-list-resolver.tsx
@@ -1,12 +1,12 @@
 import _ from 'lodash';
 import { resolve } from 'react-resolver';
-import { registrationApiGet } from '../../api/concept-catalogue-api';
+import { conceptCatalogueApiGet } from '../../api/concept-catalogue-api';
 import { publisherApiGet } from '../../api/publisher-api';
 
 const memoizedPublisherApiGetByOrgNr = _.memoize(publisherApiGet);
 
 const mapProps = {
-  concepts: props => registrationApiGet(`/begreper?orgNummer=${_.get(props, ['match', 'params', 'catalogId'])}`),
+  concepts: props => conceptCatalogueApiGet(`/begreper?orgNummer=${_.get(props, ['match', 'params', 'catalogId'])}`),
   publisher: props => memoizedPublisherApiGetByOrgNr(`/publishers/${_.get(props, ['match', 'params', 'catalogId'])}`)
 };
 

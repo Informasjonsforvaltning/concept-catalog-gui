c8fba989133282c87b73cb56637aeb8e91ce22f9
Markko Paas <markko.paas@gmail.com>
2019-08-30 12:33:18 +0200

    feat: #2136 Trigger relogin with deep backlink to self, if user does not have permissions for the selected resource

--- src/auth/auth-service.ts
+++ src/auth/auth-service.ts
@@ -3,6 +3,7 @@ import Keycloak, { KeycloakInstance } from 'keycloak-js';
 import { getConfig } from '../config';
 import { loadTokens, removeTokens, storeTokens } from './token-store';
 import get from 'lodash/get';
+import find from 'lodash/find';
 
 let kc: KeycloakInstance;
 
@@ -35,3 +36,14 @@ export const getToken = async () => {
   await kc.updateToken(5);
   return kc.token;
 };
+
+//expand authorities protocol to object structure
+const extractResourceRoles = (authorities: string) =>
+  authorities
+    .split(',')
+    .map(authorityDescriptor => authorityDescriptor.split(':'))
+    .map(parts => ({ resource: parts[0], resourceId: parts[1], role: parts[2] }));
+
+const getRoles = () => extractResourceRoles(get(kc.tokenParsed, 'authorities'));
+
+export const hasAnyRoleForResource = ({ resource, resourceId }) => !!find(getRoles(), { resource, resourceId });

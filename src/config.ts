3d44697d609d4452706091d3ce5be4855ac106d6
Markko Paas <markko.paas@gmail.com>
2019-09-13 14:29:22 +0200

    conf: Add common configurations as commented out code

--- src/config.ts
+++ src/config.ts
@@ -41,5 +41,23 @@ export const getConfig = (): Config => config;
 export const loadConfig = async (): Promise<void> => {
   const response = await axios.get('/env.json');
   const env = response.data;
+
+  // override all env variables to ut1
+  // Object.assign(env, {
+  //   REGISTRATION_HOST:'https://registrering.ut1.fellesdatakatalog.brreg.no',
+  //   CONCEPT_REGISTRATION_API:'https://registrering-begrep-api.ut1.fellesdatakatalog.brreg.no',
+  //   SSO_HOST:'https://sso.ut1.fellesdatakatalog.brreg.no',
+  //   PUBLISHER_API:'https://www.ut1.fellesdatakatalog.brreg.no',
+  //   PUBLISHER_DATA_AUTHORIZATION:'Basic ZmRrOkJSUkVH'
+  // });
+
+  // override all env variables to local docker
+  // Object.assign(env, {
+  //   REGISTRATION_HOST:'http://localhost:8098',
+  //   CONCEPT_REGISTRATION_API:'http://localhost:8200',
+  //   SSO_HOST:'http://localhost:8084',
+  //   PUBLISHER_API:'http://localhost:8080',
+  // });
+
   Object.assign(config, createConfig(env));
 };

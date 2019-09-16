42775dd9869567aeb2782631c8a3531de7ffe362
Gjermund Skobba <gjermund@skobba.net>
2019-09-16 13:11:12 +0200

    fix: Allowed null on eksempel and fagområde on validation.

--- src/pages/concept-registration-page/form-use-of-concept/form-useOfConcept.schema.tsx
+++ src/pages/concept-registration-page/form-use-of-concept/form-useOfConcept.schema.tsx
@@ -2,8 +2,12 @@ import * as Yup from 'yup';
 import { localization } from '../../../lib/localization';
 
 export const schema = Yup.object().shape({
-  eksempel: Yup.string().min(2, localization.validationMin2),
-  fagområde: Yup.string().min(2, localization.validationMin2),
+  eksempel: Yup.string()
+    .min(2, localization.validationMin2)
+    .nullable(),
+  fagområde: Yup.string()
+    .min(2, localization.validationMin2)
+    .nullable(),
   bruksområde: Yup.array().of(Yup.string().min(2, localization.validationMin2)),
   omfang: Yup.object().shape({
     tekst: Yup.string()

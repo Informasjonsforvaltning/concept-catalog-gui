3497c25a92b1575aaf68aad8ba88c3fe2280075d
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-09-30 10:27:58 +0200

    chore: remove unused route

--- src/app/routeConfig.ts
+++ src/app/routeConfig.ts
@@ -1,6 +1,5 @@
 import { ConceptListPage } from '../pages/concept-list-page/concept-list-page';
 import { ConceptRegistrationPage } from '../pages/concept-registration-page/concept-registration-page';
-import { PublisherBreadcrumb } from './breadcrumbs/publisher-breadcrumb/publisher-breadcrumb.component';
 import { localization } from '../lib/localization';
 
 export const routeConfig = [
@@ -9,12 +8,6 @@ export const routeConfig = [
     component: ConceptListPage,
     exact: true,
     breadcrumb: localization.breadCrumbsConceptCatalog
-  },
-  {
-    path: '/list',
-    component: ConceptListPage,
-    exact: true,
-    breadcrumb: props => PublisherBreadcrumb({ ...props, breadCrumbLabel: 'breadCrumbsPublisherList' })
   },
   {
     path: '/:catalogId/:conceptId',

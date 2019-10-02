0f4337f12b6bb99bbd26cf758d56a5f9058e04f4
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-10-02 10:20:32 +0200

    chore: add multilingual-field component

---
+++ src/components/multilingual-field/multilingual-field.component.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Field } from 'formik';
+
+interface Props {
+  name: string;
+  label: string;
+  component: Function;
+  languages: any;
+  showLabel?: boolean;
+}
+
+export const MultilingualField = ({ name, component, languages, label, showLabel = false }: Props): JSX.Element => (
+  <>
+    {showLabel && label && (
+      <label className="fdk-form-label w-100 pl-2" htmlFor={name}>
+        {label}
+      </label>
+    )}
+    {languages.map(({ code, selected }) => {
+      const isOnlyOneSelectedLanguage = languages.filter(({ selected }) => selected).length === 1;
+
+      return (
+        selected && (
+          <Field
+            key={code}
+            name={`${name}.${code}`}
+            label={`${label}.${code}`}
+            component={component}
+            language={code}
+            isOnlyOneSelectedLanguage={isOnlyOneSelectedLanguage}
+          />
+        )
+      );
+    })}
+  </>
+);

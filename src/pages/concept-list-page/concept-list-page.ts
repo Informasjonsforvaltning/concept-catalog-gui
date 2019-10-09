b05dbc54ad0e102bcc605a9c6d68dc9226bdfc63
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-10-09 14:59:05 +0200

    refactor: replace recompose/compose with lodash/flowRight

--- src/pages/concept-list-page/concept-list-page.ts
+++ src/pages/concept-list-page/concept-list-page.ts
@@ -1,13 +1,14 @@
-import _ from 'lodash';
-import { compose, withProps } from 'recompose';
+import pick from 'lodash/pick';
+import { withProps } from 'recompose';
+import flowRight from 'lodash/flowRight';
 
 import { ConceptListPagePure } from './concept-list-page-pure';
 import { conceptListResolver } from './concept-list-resolver';
 import { requirePermissionDecorator } from '../../auth/require-permission-decorator';
 
-const mapRouteParams = withProps(({ match: { params } }) => _.pick(params, 'catalogId'));
+const mapRouteParams = withProps(({ match: { params } }) => pick(params, 'catalogId'));
 
-const enhance = compose(
+const enhance = flowRight(
   mapRouteParams,
   requirePermissionDecorator({ resource: 'publisher', resourceIdPropName: 'catalogId' }),
   conceptListResolver

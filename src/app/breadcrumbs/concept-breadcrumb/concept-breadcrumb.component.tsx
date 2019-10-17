20d0989385eca3d03d7cb39b13677dd0ea73f699
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-10-17 08:35:00 +0200

    chore: read translateable fields

--- src/app/breadcrumbs/concept-breadcrumb/concept-breadcrumb.component.tsx
+++ src/app/breadcrumbs/concept-breadcrumb/concept-breadcrumb.component.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import get from 'lodash/get';
 import { StateConsumer } from '../../context/stateContext';
 import { localization } from '../../../lib/localization';
+import { getTranslateText } from '../../../lib/translateText';
 
 interface ConceptBreadcrumbProps {
   name: string;
@@ -13,8 +14,8 @@ export const ConceptBreadcrumb: React.FC<ConceptBreadcrumbProps> = ({ match }) =
   return (
     <StateConsumer>
       {value => {
-        const anbefaltTerm = get(value, ['statusBarState', conceptId, 'anbefaltTerm']);
-        return anbefaltTerm || localization.newConcept;
+        const anbefaltTerm = get(value, ['statusBarState', conceptId, 'anbefaltTerm', 'navn']);
+        return getTranslateText(anbefaltTerm) || localization.newConcept;
       }}
     </StateConsumer>
   );
--- src/pages/concept-registration-page/concept-registration-page-pure.tsx
+++ src/pages/concept-registration-page/concept-registration-page-pure.tsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect } from 'react';
 import get from 'lodash/get';
-import { StateContext } from '../../app/context/stateContext';
+import { StateContext, StateConsumer } from '../../app/context/stateContext';
 import './concept-registration-page-pure.scss';
 import { FormConcept } from './form-concept/form-concept.component';
 import { conceptPatchSuccessAction } from '../../app/reducers/stateReducer';
 import { Concept } from '../../domain/Concept';
-import { StateConsumer } from '../../app/context/stateContext';
+
+import { getTranslateText } from '../../lib/translateText';
 
 interface Kilde {
   uri: string;
@@ -19,7 +20,7 @@ interface Props {
 
 export const ConceptRegistrationPagePure = ({ concept }: Props): JSX.Element => {
   const { dispatch } = useContext(StateContext);
-  const anbefaltTerm = get(concept, 'anbefaltTerm');
+  const anbefaltTerm = get(concept, ['anbefaltTerm', 'navn']);
 
   useEffect(() => {
     dispatch(conceptPatchSuccessAction(concept.id, {}, concept));
@@ -28,7 +29,7 @@ export const ConceptRegistrationPagePure = ({ concept }: Props): JSX.Element => 
   return (
     <div className="container">
       <div className="col-12">
-        <h1 className="pb-5">{anbefaltTerm}</h1>
+        <h1 className="pb-5">{getTranslateText(anbefaltTerm)}</h1>
         <FormConcept concept={concept} dispatch={dispatch} />
       </div>
     </div>

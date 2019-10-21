20914e116c952ae26c337b8fae3d396aa1fb52aa
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-10-21 08:04:35 +0200

    refactor: refactor use of stateContext

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import _ from 'lodash';
 import { Header } from '../components/app-header/app-header.component';
@@ -15,28 +15,23 @@ import 'designsystemet/fdk-designsystem-bootstrap4/scss/common.scss';
 import 'designsystemet/fdk-designsystem-bootstrap4/scss/animations.scss';
 import 'designsystemet/fdk-designsystem-bootstrap4/scss/register.scss';
 import 'font-awesome/scss/font-awesome.scss';
-import { stateReducer } from './reducers/stateReducer';
 import { StateProvider } from './context/stateContext';
 
-export const App: React.FC = () => {
-  const [statusBarState, dispatch] = useReducer(stateReducer, [{}]);
-  const value = { statusBarState, dispatch };
-  return (
-    <Router>
-      <StateProvider value={value}>
-        <div className="d-flex flex-column site theme-fdk">
-          <Header />
-          <Breadcrumbs />
-          <div className="site-content d-flex flex-column pt-5">
-            <Switch>
-              {routeConfig.map((route, i) => (
-                <Route key={`${i}-${_.get(route, 'path', '')}`} {...route} />
-              ))}
-            </Switch>
-          </div>
-          <Footer />
+export const App: React.FC = () => (
+  <Router>
+    <StateProvider>
+      <div className="d-flex flex-column site theme-fdk">
+        <Header />
+        <Breadcrumbs />
+        <div className="site-content d-flex flex-column pt-5">
+          <Switch>
+            {routeConfig.map((route, i) => (
+              <Route key={`${i}-${_.get(route, 'path', '')}`} {...route} />
+            ))}
+          </Switch>
         </div>
-      </StateProvider>
-    </Router>
-  );
-};
+        <Footer />
+      </div>
+    </StateProvider>
+  </Router>
+);
---
+++ src/app/context/stateContext.tsx
@@ -0,0 +1,23 @@
+import React, { createContext, Dispatch, useContext, useReducer } from 'react';
+import { Action, stateReducer } from '../reducers/stateReducer';
+
+export const StateContext = createContext({});
+export const DispatchContext = createContext((() => {}) as Dispatch<Action>);
+
+export const StateProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(stateReducer, {});
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <StateContext.Provider value={state}>{children}</StateContext.Provider>
+    </DispatchContext.Provider>
+  );
+};
+
+export const useDispatch = () => {
+  return useContext(DispatchContext);
+};
+
+export const useGlobalState = property => {
+  const state = useContext(StateContext);
+  return state[property];
+};

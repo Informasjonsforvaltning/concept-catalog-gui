777f04a3165c2239d0cee15948c91ea298ebc63d
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-10-21 12:04:12 +0200

    refactor: add hasPermissionForResource

--- src/auth/require-permission-decorator.tsx
+++ src/auth/require-permission-decorator.tsx
@@ -1,8 +1,10 @@
 import React, { ComponentType } from 'react';
-import { hasAnyRoleForResource, logout } from './auth-service';
+import { hasPermissionForResource, logout } from './auth-service';
 
-export const requirePermissionDecorator = ({ resource, resourceIdPropName }) => (Component: ComponentType) => props => {
-  if (!hasAnyRoleForResource({ resource, resourceId: props[resourceIdPropName] })) {
+export const requirePermissionDecorator = ({ resource, resourceIdPropName, permission }) => (
+  Component: ComponentType
+) => props => {
+  if (!hasPermissionForResource({ resource, resourceId: props[resourceIdPropName], permission })) {
     // todo right now this will redirect to login url with redirect to self.
     // effectively it means that user gets chance to authenticate as another role, and continue accessing the resource.
     // we want to change it to a click-trough where user is infomed about the upcoming action with some explanation.
--- src/pages/concept-registration-page/concept-registration-page.ts
+++ src/pages/concept-registration-page/concept-registration-page.ts
@@ -4,12 +4,13 @@ import { compose, withProps } from 'recompose';
 import { ConceptRegistrationPagePure } from './concept-registration-page-pure';
 import { conceptRegistrationResolver } from './concept-registration-resolver';
 import { requirePermissionDecorator } from '../../auth/require-permission-decorator';
+import { PERMISSION_READ } from '../../auth/auth-service';
 
 const mapRouteParams = withProps(({ match: { params } }) => _.pick(params, ['catalogId', 'conceptId']));
 
 const enhance = compose(
   mapRouteParams,
-  requirePermissionDecorator({ resource: 'publisher', resourceIdPropName: 'catalogId' }),
+  requirePermissionDecorator({ resource: 'publisher', resourceIdPropName: 'catalogId', permission: PERMISSION_READ }),
   conceptRegistrationResolver
 );
 export const ConceptRegistrationPage = enhance(ConceptRegistrationPagePure);

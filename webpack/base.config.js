6d4ee26a741e29bae078aef19953897acaa4c745
Markko Paas <markko.paas@gmail.com>
2019-10-30 00:12:39 +0100

    refactor: separate out webpack base config

---
+++ webpack/base.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/index.tsx'],
+  output: {
+    path: path.join(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 10000 } // Convert images < 10k to base64 strings
+          }
+        ]
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        issuer: {
+          test: /\.tsx?$/
+        },
+        use: ['babel-loader', '@svgr/webpack', 'url-loader']
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    new CopyWebpackPlugin([{ from: './src/assets/img/*', to: './img', flatten: true }], {
+      copyUnmodified: true
+    })
+  ]
+};
--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,16 +1,9 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const merge = require('webpack-merge');
+const baseConfig = require('./base.config');
 
-module.exports = {
+module.exports = merge(baseConfig, {
   mode: 'development',
   devtool: 'cheap-module-source-map',
-  entry: ['./src/index.tsx'],
-  output: {
-    path: path.join(__dirname, '../dist'),
-    filename: 'bundle.js'
-  },
   devServer: {
     host: '0.0.0.0',
     port: 8203,
@@ -27,60 +20,5 @@ module.exports = {
           SSO_HOST: process.env.SSO_HOST || 'http://localhost:8084'
         })
       )
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.(png|jpg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: { limit: 10000 } // Convert images < 10k to base64 strings
-          }
-        ]
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        loader: 'ts-loader'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        issuer: {
-          test: /\.tsx?$/
-        },
-        use: ['babel-loader', '@svgr/webpack', 'url-loader']
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css'
-    }),
-    new CopyWebpackPlugin([{ from: './src/assets/img/*', to: './img', flatten: true }], {
-      copyUnmodified: true
-    })
-  ]
-};
+  }
+});

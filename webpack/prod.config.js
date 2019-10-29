258d3ede9f97cfc5574587a86085fda49a41ead9
Markko Paas <markko.paas@gmail.com>
2019-10-29 23:57:59 +0100

    refactor: move webpack files under webpack directory

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -4,29 +4,12 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'cheap-module-source-map',
+  mode: 'production',
+  devtool: 'none',
   entry: ['./src/index.tsx'],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    host: '0.0.0.0',
-    port: 8203,
-    historyApiFallback: true,
-    before: app =>
-      app.get('/env.json', (_, res) =>
-        res.json({
-          CONCEPT_REGISTRATION_API: process.env.CONCEPT_REGISTRATION_API || 'http://localhost:8200',
-          REGISTRATION_HOST: process.env.REGISTRATION_HOST || 'https://localhost:8098',
-          PUBLISHER_API: process.env.PUBLISHER_API || 'http://localhost:8080',
-          PUBLISHER_DATA_AUTHORIZATION: process.env.PUBLISHER_DATA_AUTHORIZATION || '',
-          // PUBLISHER_API:'https://www.ut1.fellesdatakatalog.brreg.no'
-          // PUBLISHER_DATA_AUTHORIZATION:'Basic ZmRrOkJSUkVH' // for ut1 and st1
-          SSO_HOST: process.env.SSO_HOST || 'http://localhost:8084'
-        })
-      )
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].[contenthash].js'
   },
   module: {
     rules: [

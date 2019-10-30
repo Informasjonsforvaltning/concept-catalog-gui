2cb534e813ebd990d839ae7541ef31a1254a34f7
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-10-30 14:09:29 +0100

    refactor: split patch-operation from validate-operation

--- src/lib/validateConceptForm.ts
+++ src/lib/validateConceptForm.ts
@@ -1,11 +1,9 @@
 import _ from 'lodash';
 import { validateYupSchema } from 'formik';
 import { conceptValidationErrorAction } from '../app/reducers/stateReducer';
-import { patchConceptFromForm } from './patchConceptForm';
 
 export const validateConceptForm = (values, schema, concept, dispatch): void => {
   validateYupSchema(values, schema)
     .then(() => dispatch(conceptValidationErrorAction(_.get(concept, 'id'), false)))
     .catch(() => dispatch(conceptValidationErrorAction(_.get(concept, 'id'), true)));
-  patchConceptFromForm(values, { concept, dispatch });
 };

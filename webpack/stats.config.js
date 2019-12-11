ba2aa747e564cbfcdd6634a6993b86577e354005
Markko Paas <markko.paas@gmail.com>
2019-12-11 16:33:13 +0100

    chore: add npm command build:webpack:stats

---
+++ webpack/stats.config.js
@@ -0,0 +1,13 @@
+import merge from 'webpack-merge';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+import prodConfig from './prod.config';
+
+export default merge(prodConfig, {
+  plugins: [
+    new BundleAnalyzerPlugin({
+      openAnalyzer: true,
+      generateStatsFile: true
+    })
+  ]
+});

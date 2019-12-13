683432ca6d519bd7eca23ffb3ae0e18801af5d86
Markko Paas <markko.paas@gmail.com>
2019-12-13 13:31:26 +0100

    refactor: replace specific requirePermissionDecorator with more generic requirePredicate

---
+++ src/lib/require-predicate.tsx
@@ -0,0 +1,11 @@
+import React, { ComponentType } from 'react';
+
+export const requirePredicate = (predicate: (props: any) => boolean, elseHandler: () => void) => (
+  Component: ComponentType
+) => props => {
+  if (!predicate(props)) {
+    elseHandler();
+    return null;
+  }
+  return <Component {...props} />;
+};

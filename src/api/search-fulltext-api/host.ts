b7464bbd603f8175cf2b6f8a52d0eadc7d462e38
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-09-14 11:38:57 +0200

    feat: #226 resolve concepts from fulltext-search concept endpoint

--- src/api/search-fulltext-api/host.ts
+++ src/api/search-fulltext-api/host.ts
@@ -5,16 +5,21 @@ import { getConfig } from '../../config';
 interface Props {
   path: string;
   method: any;
-  params: any;
+  params?: any;
+  data?: any;
 }
 
-export const searchFullTextApi = ({ path, method, params }: Props) =>
+export const searchFullTextApi = ({ path, method, params, data }: Props) =>
   axios({
     url: `${getConfig().searchFullTextApi.host}${path}`,
     method,
-    params
+    params,
+    data
   })
     .then(response => cleanDeep(response.data))
     .catch(() => null);
+
+export const searchFullTextApiPost = (path: string, body: any) =>
+  searchFullTextApi({ path, method: 'POST', data: body });
 
 export const searchFullTextApiGet = (path: string, params: any) => searchFullTextApi({ path, method: 'GET', params });

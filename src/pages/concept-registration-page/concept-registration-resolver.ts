8c586754eb049bc1d18fb74226aef3f3bddf0740
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-09-21 21:22:55 +0200

    refactor(concept-registration-page): fetch concept by useEffect

--- src/pages/concept-registration-page/concept-registration-resolver.ts
+++
@@ -1,8 +0,0 @@
-import { resolve } from 'react-resolver';
-import { getConcept } from '../../api/concept-catalogue-api';
-
-const mapProps = {
-  concept: ({ conceptId }) => getConcept(conceptId)
-};
-
-export const conceptRegistrationResolver = resolve(mapProps);

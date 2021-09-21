38e4cb6db9a61935d6296c518d3f235b8aee4e89
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-09-21 08:25:15 +0200

    refactor(registration-page,form-concept): move concept title

--- src/pages/concept-registration-page/styled.ts
+++ src/pages/concept-registration-page/styled.ts
@@ -4,12 +4,8 @@ import { theme } from '@fellesdatakatalog/theme';
 
 import ContainerSC from '../../components/container/styled';
 
-const Title = styled.h1`
-  font-size: ${theme.fontSize('FS48')};
-`;
-
 const Container = styled(ContainerSC.Container)`
   padding-top: ${theme.spacing('S48')};
 `;
 
-export default { Title, Container };
+export default { Container };

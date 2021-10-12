73eafe25c885271cf7540572b6f42e0a433a481d
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-10-12 13:39:34 +0200

    chore: update webpack and address issues

---
+++ webpack/dev.config.ts
@@ -0,0 +1,126 @@
+import type { Configuration as WebpackConfiguration } from 'webpack';
+import type { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import { resolve } from 'path';
+import {
+  mergeWithCustomize,
+  customizeArray,
+  CustomizeRule
+} from 'webpack-merge';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+import baseConfig from './base.config';
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const configuration: Configuration = mergeWithCustomize<Configuration>({
+  customizeArray: customizeArray({
+    'module.rules': CustomizeRule.Replace
+  })
+})(baseConfig, {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 8203,
+    hot: true,
+    onBeforeSetupMiddleware: devServer =>
+      devServer.app.get('/config.js', (_, res) => res.status(204).send()),
+    historyApiFallback: {
+      rewrites: [{ from: /./, to: '/index.html' }]
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              configFile: resolve(__dirname, '../babel.config.js'),
+              plugins: ['react-refresh/babel']
+            }
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: resolve(__dirname, '../tsconfig.json'),
+              transpileOnly: true
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'react-svg-loader',
+            options: {
+              jsx: true
+            }
+          }
+        ],
+        include: [resolve(__dirname, '..', 'src', 'images')]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ],
+        include: [
+          resolve(__dirname, '../node_modules/ansi-styles'),
+          resolve(__dirname, '../node_modules/chalk'),
+          resolve(__dirname, '../node_modules/react-dev-utils')
+        ]
+      },
+      {
+        test: /\.(js|ts)x?$/,
+        use: ['source-map-loader'],
+        enforce: 'pre',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        type: 'asset/inline',
+        exclude: [resolve(__dirname, '..', 'src', 'images')]
+      }
+    ]
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: { files: './src/**/*.{tsx,ts,jsx,js,json,html}' }
+    })
+  ]
+});
+
+export default configuration;

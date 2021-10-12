ad08acb3c2f456537b152f9dc4a8b9b2fcb1af7d
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-10-12 13:41:56 +0200

    refactor: move styles import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,14 +9,6 @@ import { routeConfig } from './routeConfig';
 import { Breadcrumbs } from '../components/breadcrumbs/breadcrumbs.component';
 import { authService } from '../services/auth-service';
 
-import './styles/bootstrap-override.scss';
-import 'designsystemet/fdk-designsystem-bootstrap4/scss/helper.scss';
-import 'designsystemet/fdk-designsystem-bootstrap4/scss/portal.scss';
-import 'designsystemet/fdk-designsystem-bootstrap4/scss/typo.scss';
-import 'designsystemet/fdk-designsystem-bootstrap4/scss/common.scss';
-import 'designsystemet/fdk-designsystem-bootstrap4/scss/animations.scss';
-import 'designsystemet/fdk-designsystem-bootstrap4/scss/register.scss';
-import 'font-awesome/scss/font-awesome.scss';
 import { StateProvider } from './context/stateContext';
 import { getConfig } from '../config';
 
--- src/app/styles/index.ts
+++ src/app/styles/index.ts
@@ -1,5 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
+import './bootstrap-override.scss';
+
+import 'designsystemet/fdk-designsystem-bootstrap4/scss/helper.scss';
+import 'designsystemet/fdk-designsystem-bootstrap4/scss/portal.scss';
+import 'designsystemet/fdk-designsystem-bootstrap4/scss/typo.scss';
+import 'designsystemet/fdk-designsystem-bootstrap4/scss/common.scss';
+import 'designsystemet/fdk-designsystem-bootstrap4/scss/animations.scss';
+import 'designsystemet/fdk-designsystem-bootstrap4/scss/register.scss';
+
+import 'font-awesome/scss/font-awesome.scss';
+
 import CommonStyles from './common';
 
 export default createGlobalStyle`

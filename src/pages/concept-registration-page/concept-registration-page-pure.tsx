908f6e695ea0823aa47ae6549cfb03ce1a7e5c74
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-11-17 09:27:18 +0100

    feat: add comments

--- src/pages/concept-registration-page/concept-registration-page-pure.tsx
+++ src/pages/concept-registration-page/concept-registration-page-pure.tsx
@@ -10,12 +10,21 @@ import './concept-registration-page-pure.scss';
 import { FormConcept } from './form-concept';
 import { conceptPatchSuccessAction } from '../../app/reducers/stateReducer';
 
+import { useAppSelector, useAppDispatch } from '../../app/redux/hooks';
+import {
+  fetchComments,
+  commentActions,
+  selectAllComments
+} from '../../features/comments';
+
 import Root from '../../components/root';
 
 import SC from './styled';
 import { getConcept } from '../../api/concept-catalogue-api';
+import { CommentList } from '../../components/comment-list';
 
 interface RouteParams {
+  catalogId: string;
   conceptId: string;
 }
 
@@ -23,12 +32,16 @@ interface Props extends RouteComponentProps<RouteParams> {}
 
 const ConceptRegistrationPagePure: FC<Props> = ({
   match: {
-    params: { conceptId }
+    params: { catalogId, conceptId }
   }
 }) => {
   const [concept, setConcept] = useState<Concept>();
   const globalStateValues = useGlobalState(conceptId);
   const dispatch = useDispatch();
+
+  const appDispatch = useAppDispatch();
+  const comments = useAppSelector(selectAllComments);
+  const { resetComments } = commentActions;
 
   useEffect(() => {
     getConcept(conceptId).then(fetchedConcept => {
@@ -36,7 +49,17 @@ const ConceptRegistrationPagePure: FC<Props> = ({
       dispatch(
         conceptPatchSuccessAction(fetchedConcept.id, false, fetchedConcept)
       );
+      fetchedConcept.originaltBegrep &&
+        appDispatch(
+          fetchComments({
+            orgNumber: catalogId,
+            topicId: fetchedConcept.originaltBegrep
+          })
+        );
     });
+    return () => {
+      appDispatch(resetComments());
+    };
   }, [conceptId]);
 
   return (
@@ -48,6 +71,13 @@ const ConceptRegistrationPagePure: FC<Props> = ({
             dispatch={dispatch}
             lastPatchedResponse={get(globalStateValues, 'lastPatchedResponse')}
             isSaving={globalStateValues.isSaving}
+          />
+        )}
+        {concept?.originaltBegrep && (
+          <CommentList
+            catalogId={catalogId}
+            topicId={concept.originaltBegrep}
+            comments={comments}
           />
         )}
       </SC.Container>

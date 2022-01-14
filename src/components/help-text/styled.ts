d79cec1dcf860249a7b245dd68dd09f4cfbaa01f
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-01-14 15:40:32 +0100

    fix: update HelpText styling

--- src/components/help-text/styled.ts
+++ src/components/help-text/styled.ts
@@ -1,9 +1,35 @@
 import styled from 'styled-components';
 
-import { theme } from '@fellesdatakatalog/theme';
+import { Colour, theme } from '@fellesdatakatalog/theme';
+
+const HelpText = styled.div`
+  background-color: ${theme.colour(Colour.CYAN, 'C20')};
+  border-radius: 5px;
+  font-size: ${theme.fontSize('FS14')};
+  margin-bottom: ${theme.spacing('S12')};
+  padding: ${theme.spacing('S10')};
+`;
 
 const Title = styled.h3`
   font-size: ${theme.fontSize('FS20')};
 `;
 
-export default { Title };
+const Required = styled.div`
+  background-color: ${theme.colour(Colour.GREEN, 'G60')};
+  border-radius: 20px;
+  color: ${theme.colour(Colour.NEUTRAL, 'N0')};
+  margin-left: ${theme.spacing('S6')};
+  padding: 0 ${theme.spacing('S6')};
+`;
+
+const ShowMoreButton = styled.button`
+  align-self: flex-start;
+  background-color: transparent;
+  border: none;
+  color: ${theme.colour(Colour.GREEN, 'G60')};
+  margin-left: ${theme.spacing('S6')};
+  text-decoration: underline;
+  white-space: nowrap;
+`;
+
+export default { HelpText, Title, Required, ShowMoreButton };

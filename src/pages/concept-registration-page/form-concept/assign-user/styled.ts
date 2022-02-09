94d4d864ca09d48cc561e8b9e73603cf9a1f606c
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-02-09 14:27:22 +0100

    style(assign-user): minor style change

--- src/pages/concept-registration-page/form-concept/assign-user/styled.ts
+++ src/pages/concept-registration-page/form-concept/assign-user/styled.ts
@@ -2,21 +2,15 @@ import styled from 'styled-components';
 import { Colour, theme } from '@fellesdatakatalog/theme';
 
 const AssignUser = styled.div`
-  border: 2px dashed ${theme.colour(Colour.GREEN, 'G50')};
-  border-radius: 5px;
   display: flex;
   flex-direction: column;
   margin-top: ${theme.spacing('S12')};
   padding: ${theme.spacing('S24')};
 
-  & > div {
-    width: 75%;
-
-    & > div > label > input {
-      border-color: ${theme.colour(Colour.NEUTRAL, 'N70')};
-      font-size: ${theme.fontSize('FS20')};
-      height: 3em;
-    }
+  & > div > div > label > input {
+    border-color: ${theme.colour(Colour.NEUTRAL, 'N70')};
+    font-size: ${theme.fontSize('FS20')};
+    height: 3em;
   }
 `;
 

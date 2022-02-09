37ba123e1c7c76adcb8b1b0bc6c86d177ea279e0
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-02-09 12:13:27 +0100

    refactor: api suggestions add return type

--- src/api/search-fulltext-api/suggestions.ts
+++ src/api/search-fulltext-api/suggestions.ts
@@ -1,7 +1,9 @@
 import { searchFullTextApiGet } from './host';
+import { SkosConcept } from '../../types';
 
-export const extractSuggestions = (searchResponse: any) =>
-  searchResponse.suggestions ?? [];
+export const extractSuggestions = (
+  searchResponse: any
+): Promise<SkosConcept[]> => searchResponse.suggestions ?? [];
 
 export const getConceptSuggestions = (params: any) =>
   searchFullTextApiGet('/suggestion/concepts', params);

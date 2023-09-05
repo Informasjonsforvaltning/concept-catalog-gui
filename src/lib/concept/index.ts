50d9febc240aa77c66ec23d312eb46e4e5ba1432
NilsOveTen <github.nils.ove@tendenes.com>
2023-09-05 12:15:28 +0200

    fix: rm ConceptStatus from editable check

--- src/lib/concept/index.ts
+++ src/lib/concept/index.ts
@@ -1,17 +1,9 @@
 import { Concept } from '../../types';
-import { ConceptStatus } from '../../types/enums';
 
 export const isConceptEditable = (concept: Concept | undefined) => {
   if (!concept) {
     return false;
   }
-  const { status, erPublisert, erSistPublisert, revisjonAvSistPublisert } =
-    concept;
-  return (
-    (erPublisert && erSistPublisert) ||
-    ((status === ConceptStatus.UTKAST ||
-      status === ConceptStatus.HOERING ||
-      status === ConceptStatus.GODKJENT) &&
-      revisjonAvSistPublisert)
-  );
+  const { erPublisert, erSistPublisert, revisjonAvSistPublisert } = concept;
+  return (erPublisert && erSistPublisert) || revisjonAvSistPublisert;
 };

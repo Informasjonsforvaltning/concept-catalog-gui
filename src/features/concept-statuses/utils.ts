4134d50c483ad277694fdb28c404fd2e4199f022
NilsOveTen <github.nils.ove@tendenes.com>
2023-09-22 10:02:57 +0200

    refactor: simplify prepareStatusList

--- src/features/concept-statuses/utils.ts
+++ src/features/concept-statuses/utils.ts
@@ -1,30 +1,6 @@
 import type { ReferenceDataCode } from '../../types';
 
 export const prepareStatusList = (conceptStatuses: ReferenceDataCode[]) => {
-  const utilizedCodes: ReferenceDataCode[] = [];
-
-  const draft = conceptStatuses.find(code => code.code === 'DRAFT');
-  if (draft) utilizedCodes.push(draft);
-
-  const candidate = conceptStatuses.find(code => code.code === 'CANDIDATE');
-  if (candidate) utilizedCodes.push(candidate);
-
-  const waiting = conceptStatuses.find(code => code.code === 'WAITING');
-  if (waiting) {
-    waiting.label = {
-      en: 'waiting',
-      nb: 'til godkjenning',
-      nn: 'til godkjenning'
-    };
-    utilizedCodes.push(waiting);
-  }
-
-  const current = conceptStatuses.find(code => code.code === 'CURRENT');
-  if (current) utilizedCodes.push(current);
-
-  const retired = conceptStatuses.find(code => code.code === 'RETIRED');
-  if (retired) utilizedCodes.push(retired);
-
   const rejected = {
     uri: 'internal codes - REJECTED',
     code: 'REJECTED',
@@ -34,7 +10,27 @@ export const prepareStatusList = (conceptStatuses: ReferenceDataCode[]) => {
       nn: 'avvist'
     }
   } as ReferenceDataCode;
-  utilizedCodes.push(rejected);
+
+  const editedStatuses = conceptStatuses.map(code => {
+    if (code.code === 'WAITING') {
+      code.label = {
+        en: 'waiting',
+        nb: 'til godkjenning',
+        nn: 'til godkjenning'
+      };
+    }
+    return code;
+  });
+  editedStatuses.push(rejected);
+
+  const utilizedCodes: ReferenceDataCode[] = [];
+
+  ['DRAFT', 'CANDIDATE', 'WAITING', 'CURRENT', 'RETIRED', 'REJECTED'].forEach(
+    code => {
+      const utilized = editedStatuses.find(status => status.code === code);
+      if (utilized) utilizedCodes.push(utilized);
+    }
+  );
 
   return utilizedCodes;
 };

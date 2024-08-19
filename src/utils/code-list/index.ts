6e4d06862a721e3e7d7f5f1451e7c159312925bc
Jeff Reiffers <jeffreiffers@gmail.com>
2024-08-19 08:45:26 +0200

    fix: nodes with parent not found in accumulator got misplaced

--- src/utils/code-list/index.ts
+++ src/utils/code-list/index.ts
@@ -20,21 +20,52 @@ const findParent = (id: number, nodes: TreeNode[]) => {
 };
 
 export const convertCodeListToTreeNodes = (codeList: CodeList): TreeNode[] =>
-  codeList?.codes?.reduce((accumulator, currentValue) => {
-    const parent = findParent(currentValue.parentID, accumulator);
-    if (parent) {
-      parent.children = [
-        ...(parent.children ?? []),
-        {
-          value: `${currentValue.id}`,
-          label: getTranslateText(currentValue.name)
+  codeList?.codes?.reduce((accumulator, currentValue, _currentIndex, codes) => {
+    // Use the tree node if it already exists in the accumulator
+    const current = accumulator.find(
+      node => node.value === `${currentValue.id}`
+    );
+    // Remove the node from the accumulator if it exists
+    accumulator = accumulator.filter(
+      node => node.value !== `${currentValue.id}`
+    );
+
+    if (
+      currentValue.parentID !== null &&
+      currentValue.parentID !== 'noParent'
+    ) {
+      let parent = findParent(currentValue.parentID, accumulator);
+      if (!parent) {
+        const parentCode = codes.find(
+          code => code.id === currentValue.parentID
+        );
+        parent = parentCode
+          ? ({
+              value: `${parentCode.id}`,
+              label: `${getTranslateText(parentCode.name)}`
+            } as TreeNode)
+          : null;
+        if (parent) {
+          accumulator.push(parent);
         }
-      ];
+      }
+
+      if (parent) {
+        parent.children = [
+          ...(parent.children ?? []),
+          current ?? {
+            value: `${currentValue.id}`,
+            label: `${getTranslateText(currentValue.name)}`
+          }
+        ];
+      }
     } else {
-      accumulator.push({
-        value: `${currentValue.id}`,
-        label: getTranslateText(currentValue.name)
-      });
+      accumulator.push(
+        current ?? {
+          value: `${currentValue.id}`,
+          label: `${getTranslateText(currentValue.name)}`
+        }
+      );
     }
     return accumulator;
   }, []) ?? [];
